9ff340b8f8fdf955f7a3a3ac0ec53772d191ec68
programmaraditya123 <officialadi4255@gmail.com>
2026-03-31 11:47:27 +0530

    setup frontend and backend not folder srtructure 11:47 31-03-2026

--- backend/Server.js
+++ backend/Server.js
@@ -1,1 +1,14 @@
-// #jgcubvjfhuih
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
